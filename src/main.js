04a4a1ec25d794a398f2873c4596020d4013c4be
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-06 19:03:34 +0100

    Added FontAwesome Solid icons

--- src/main.js
+++ src/main.js
@@ -7,8 +7,17 @@ import 'buefy/dist/buefy.css';
 import DefaultLayout from '~/layouts/Default.vue';
 
 export default function(Vue, { router, head, isClient }) {
+  // Push Icons
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css'
+  });
+
   // Use Buefy
-  Vue.use(Buefy);
+  Vue.use(Buefy, {
+    defaultIconPack: 'fas' // Font Awesome Solid
+  });
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
 }
