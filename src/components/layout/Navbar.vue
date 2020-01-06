bbeb8034bd1b6362de8cd62b4deefacfb20532f0
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-06 20:57:42 +0100

    Started working

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,7 +1,62 @@
 <template>
-  <div class="p-5 bg-red-500">
-    Brand
-  </div>
+  <nav class="py-2 px-5">
+    <div class="flex flex-row justify-between">
+      <!-- Right -->
+      <ul class="flex flex-row">
+        <!-- Home -->
+        <li class="px-2">
+          <g-link to="/home/">Home</g-link>
+        </li>
+
+        <!-- Browse Fursonas -->
+        <li class="px-2">
+          <g-link to="/browse/">Browse Fursonas</g-link>
+        </li>
+
+        <!-- Rules -->
+        <li class="px-2">
+          <g-link to="/rules/">Rules</g-link>
+        </li>
+
+        <!-- Events -->
+        <li class="px-2">
+          <g-link to="/events/">Events</g-link>
+        </li>
+
+        <!-- Faq -->
+        <li class="px-2">
+          <g-link to="/faq/">Faq</g-link>
+        </li>
+
+        <!-- Staff -->
+        <li class="px-2">
+          <g-link to="/staff/">Staff</g-link>
+        </li>
+
+        <!-- Ban appeal -->
+        <li class="px-2">
+          <g-link to="/appeal/">Ban appeal</g-link>
+        </li>
+
+        <!-- Patreon -->
+        <li class="px-2">
+          <g-link to="/patreon/">Patreon</g-link>
+        </li>
+      </ul>
+
+      <ul class="flex flex-row">
+        <!-- Join Us -->
+        <li class="px-2">
+          <a href="https://discord.gg/BmZFfB6">Join Us</a>
+        </li>
+
+        <!-- Join Us -->
+        <li class="px-2">
+          <a href="https://discord.gg/BmZFfB6">Join Us</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="dark ">
     <!-- <header class="header">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
