2ebf122bf5042eb038b8ed1eefe8e0c4ec7363ca
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-06 19:57:51 +0100

    Testing layout and components

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="layout">
-    <header class="header">
+  <div>
+    <!-- <header class="header">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
@@ -8,43 +8,18 @@
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
-    </header>
-    <slot/>
+    </header> -->
+
+    <Navbar />
+
+    <slot />
   </div>
 </template>
 
-<static-query>
-query {
-  metadata {
-    siteName
-  }
-}
-</static-query>
+<script>
+import Navbar from '~/components/layout/Navbar.vue'
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
+export default {
+  components: { Navbar },
 }
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import Buefy from 'buefy';
-import 'buefy/dist/buefy.css';
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
-import DefaultLayout from '~/layouts/Default.vue';
+import DefaultLayout from '~/layouts/Default.vue'
 
 export default function(Vue, { router, head, isClient }) {
   // Push Icons
   head.link.push({
     rel: 'stylesheet',
-    href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css'
-  });
+    href:
+      'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-1/css/solid.min.css',
+  })
 
   // Use Buefy
   Vue.use(Buefy, {
-    defaultIconPack: 'fas' // Font Awesome Solid
-  });
+    defaultIconPack: 'fas', // Font Awesome Solid
+  })
 
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout);
+  Vue.component('Layout', DefaultLayout)
 }
