e7b779d69b19f9fa04589fa95d7748bc6777c263
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-07 12:30:34 +0100

    Changed from manual tailwind to plugin tailwind

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,28 +4,28 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-// Tailwind and PurgeCSS
-const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
-const postcssPlugins = [tailwind()]
-
-// PurgeCSS only enabled on production
-if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
-
 module.exports = {
   siteName: 'Furry kingdom',
   siteUrl: 'https://furrykingdom.xyz',
   siteDescription:
     "Rushy's Kingdom is a furry server designed with fun, freedom, and diversity in mind. Whoever and whatever you are, Rushy's Kingdom encourages you to show your true colors and be yourself! Enter the kingdom and become a part of this welcoming family of interesting characters. We encourage you to join us  and make some awesome and interesting memories with us.",
   titleTemplate: '%s | Furry kingdom',
-  plugins: [],
 
   // Tailwind and PurgeCSS
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: postcssPlugins,
-      },
+  plugins: [
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      /* These are the default options. You don't need to set any options to get going.
+      options: {
+        tailwindConfig: './some/file/js',
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
+        shouldImport: true,
+        shouldTimeTravel: true,
+        shouldPurgeUnusedKeyframes: true,
+      }
+      */
     },
-  },
+  ],
 }
