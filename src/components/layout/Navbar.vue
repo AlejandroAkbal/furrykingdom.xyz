12b37be8057bacefbe351a644e908f20b632cdda
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-07 18:53:53 +0100

    Added discord SVG

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -8,7 +8,7 @@
           src="@/assets/img/logo_white.png"
           height="64"
           width="64"
-          quality="50"
+          quality="80"
         />
       </g-link>
 
@@ -88,9 +88,22 @@
         <li class="m-auto">
           <a href="https://discord.gg/BmZFfB6">
             <button
-              class="text-default-text border-0 rounded-full bg-discord py-2 px-4 shadow hover:shadow-md"
+              class="text-default-text flex border-0 rounded-full bg-discord py-2 px-4 shadow hover:shadow-md"
             >
-              Login with Discord
+              <svg
+                class="icon w-6 h-6 pr-1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 245 240"
+              >
+                <path
+                  d="M104 104c-5 0-10 5-10 11s5 11 10 11c6 0 11-5 11-11s-5-11-11-11zm37 0c-6 0-10 5-10 11s4 11 10 11 10-5 10-11-4-11-10-11z"
+                />
+                <path
+                  d="M190 20H56c-12 0-21 9-21 21v135c0 11 9 20 21 20h113l-5-18 12 12 13 11 21 19V41c0-12-9-21-20-21zm-39 131l-7-9c13-3 18-11 18-11l-11 5c-5 3-10 4-15 5a70 70 0 01-40-5c-3 0-5-2-8-3l-1-1-3-2c0 1 5 9 18 12l-7 9c-22-1-30-16-30-16 0-32 14-58 14-58 14-11 28-10 28-10l1 1c-18 5-26 13-26 13l6-3c10-5 19-6 22-6h2a85 85 0 0151 9s-8-8-25-13l1-1s14-1 28 10c0 0 14 26 14 58 1 0-8 15-30 16z"
+                />
+              </svg>
+
+              Login
             </button>
           </a>
         </li>
