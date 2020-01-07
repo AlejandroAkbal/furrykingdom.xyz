a24b6ca7527df8845bcc7ab2339d2dd910d84c59
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-07 21:05:02 +0100

    Moved from gridstome to Nuxt

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module',
+    parser: 'babel-eslint'
   },
-  extends: ['prettier', 'prettier/vue', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+    'prettier'
+  ],
   // add your custom rules here
   rules: {
-    'prettier/prettier': 'error',
-    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
+  }
 }
