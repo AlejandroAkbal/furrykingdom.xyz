922e153841dfb099e5c9240a661d113dc3ebc1be
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-09 14:32:47 +0100

    Added scroll to top button

---
+++ components/layout/ScrollToTop.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fixed right-0 bottom-0 mr-12 mb-12">
+    <button
+      @click="scrollToTop"
+      class="py-1 md:py-2 px-2 md:px-4 text-white text-xl text-center bg-background-hover border rounded-full border-border-hover hover:border-border focus:border-border"
+    >
+      <b>&#94;</b>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+
+  methods: {
+    // Scroll to top button
+    scrollToTop() {
+      window.scrollTo(0, 0)
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -12,17 +12,21 @@
     <footer>
       <Footer />
     </footer>
+
+    <!-- Scroll to top -->
+    <ScrollToTop />
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/layout/Navbar.vue'
 import Footer from '~/components/layout/Footer.vue'
+import ScrollToTop from '~/components/layout/ScrollToTop.vue'
 
 export default {
   name: 'DefaultLayout',
 
-  components: { Navbar, Footer },
+  components: { Navbar, Footer, ScrollToTop },
 
   // Set theme and background color in the body dynamically thanks to the vuex store computed property
   head() {
