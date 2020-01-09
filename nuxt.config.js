32fb63a0d496d6f77b46a059beccc79db130adb5
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-09 12:42:36 +0100

    Added analyzer and  tweaked title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - Furry kingdom',
+    titleTemplate: "%s - Rushy's Furry Kingdom",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
