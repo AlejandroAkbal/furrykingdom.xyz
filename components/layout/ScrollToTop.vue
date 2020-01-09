18cce01785978f06daaca2246e0dca7f0551f30a
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-09 14:36:54 +0100

    Added shadow to scroll button

--- components/layout/ScrollToTop.vue
+++ components/layout/ScrollToTop.vue
@@ -2,7 +2,7 @@
   <div class="fixed right-0 bottom-0 mr-12 mb-12">
     <button
       @click="scrollToTop"
-      class="py-1 md:py-2 px-2 md:px-4 text-white text-xl text-center bg-background-hover border rounded-full border-border-hover hover:border-border focus:border-border"
+      class="py-1 md:py-2 px-2 md:px-4 text-white text-xl text-center bg-background-hover border rounded-full border-border-hover hover:border-border focus:border-border shadow-lg"
     >
       <b>&#94;</b>
     </button>
