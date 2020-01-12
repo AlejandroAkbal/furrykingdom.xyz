9eb49e69bfc6a2461ce99cc146e880b9fb30c378
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-12 12:07:14 +0100

    Completed Faq

---
+++ components/content/FaqCollapsible.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="w-full md:w-1/2 mx-auto text-default-text border border-border rounded-lg overflow-hidden"
+  >
+    <div class="bg-depth p-3">
+      <!-- Toggler -->
+      <button @click="isActive = !isActive" class="w-full flex justify-between">
+        <!-- Title -->
+        <h3 v-text="title" class="font-bold" />
+        <!-- Dinamic icon -->
+        <span v-html="isActive ? '&#9650;' : '&#9660;'" class="colorful"></span>
+      </button>
+    </div>
+    <!-- Content -->
+    <div v-if="isActive" class="bg-depth-hover px-5 py-3">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FaqCollapsible',
+
+  props: {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    active: { type: Boolean, required: false, default: false }
+  },
+
+  data() {
+    return { isActive: false }
+  },
+
+  created() {
+    this.isActive = this.active
+  }
+}
+</script>
--- pages/faq.vue
+++ pages/faq.vue
@@ -4,21 +4,22 @@
     <HeroCover title="Frequently Asked Questions" class="bg-gradient" />
 
     <!-- Page content -->
-    <div class="mx-auto w-5/6 md:w-2/3">
-      <!-- Separator -->
-      <!-- <Separator :quote="true" /> -->
+    <div class="mx-auto w-full md:w-2/3 p-6">
+      <FaqCollapsible title="What is the community about?">
+        About fun, freedom and diversity
+      </FaqCollapsible>
     </div>
   </div>
 </template>
 
 <script>
 import HeroCover from '~/components/content/HeroCover.vue'
-import Separator from '~/components/content/Separator.vue'
+import FaqCollapsible from '~/components/content/FaqCollapsible.vue'
 
 export default {
   name: 'Faq',
 
-  components: { HeroCover, Separator },
+  components: { HeroCover, FaqCollapsible },
 
   metaInfo: {
     title: 'Frequently asked questions'
