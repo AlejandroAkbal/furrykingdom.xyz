9f2e7adbe7afdff7702543639c7e2290e6ba586f
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2020-01-25 13:06:52 +0100

    Revert "Fixes"
    This reverts commit 50f56ad97936b24bc6933dc3f5ef7edf5b008be7.

---
+++ pages/faq.vue
@@ -0,0 +1,40 @@
+<template>
+  <div style="min-height: 87.5vh;">
+    <!-- Hero text -->
+    <HeroCover title="Frequently Asked Questions" class="bg-gradient" />
+
+    <!-- Page content -->
+    <div class="mx-7vw md:mx-14vw py-3">
+      <!-- Question -->
+      <FaqCollapsible>
+        <template slot="title">What is the community about?</template>
+        <template slot="description">
+          About fun, freedom and diversity.
+        </template>
+      </FaqCollapsible>
+
+      <!-- Question -->
+      <FaqCollapsible>
+        <template slot="title">How are you different from others?</template>
+        <template slot="description">
+          Because we're a kingdom.
+        </template>
+      </FaqCollapsible>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeroCover from '~/components/content/HeroCover.vue'
+import FaqCollapsible from '~/components/content/FaqCollapsible.vue'
+
+export default {
+  name: 'Faq',
+
+  components: { HeroCover, FaqCollapsible },
+
+  metaInfo: {
+    title: 'Frequently asked questions'
+  }
+}
+</script>
